493bf4bbeb4de706a0e1e254ef1d8ca8050ecf05
Ronen <Ronen>
2016-07-12 11:44:20 +0300

    showing image and sqs

--- src/assets/dragdropfile/script.js
+++ src/assets/dragdropfile/script.js
@@ -1,109 +1,115 @@
-$(document).ready(function(){
-	var dropbox;
-
-var oprand = {
-	dragClass : "active",
+$(document).ready(function () {
+  var dropbox;
+  var _URL = window.URL;
+  var oprand = {
+    dragClass: "active",
     on: {
-        load: function(e, file) {
-			// check file type
-			var imageType = /image.*/;
-			if (!file.type.match(imageType)) {
-			  alert("File \""+file.name+"\" is not a valid image file");
-			  return false;
-			}
+      load: function (e, file) {
 
-			// check file size
-			if (parseInt(file.size / 1024) > 6050) {
-			  alert("File \""+file.name+"\" is too big.Max allowed size is 2 MB.");
-			  return false;
-			}
+        // check file type
+        var imageType = /image.*/;
+        if (!file.type.match(imageType)) {
+          alert("File \"" + file.name + "\" is not a valid image file");
+          return false;
+        }
+        // check file size
+        if (parseInt(file.size / 1024) > 6050) {
+          alert("File \"" + file.name + "\" is too big.Max allowed size is 2 MB.");
+          return false;
+        }
 
-			create_box(e,file);
-    	},
+        var img = new Image();
+
+        img.onload = function () {
+          console.log("Width:" + this.width + "   Height: " + this.height);//this will give you image width and height and you can easily validate here....
+          var s = {w: this.width, h: this.height};
+          create_box(e, file, s);
+        };
+        img.src = _URL.createObjectURL(file);
+      },
     }
-};
+  };
 
-	FileReaderJS.setupDrop(document.getElementById('dropbox'), oprand);
+  FileReaderJS.setupDrop(document.getElementById('dropbox'), oprand);
 
 });
 
-create_box = function(e,file){
-	var rand = Math.floor((Math.random()*100000)+3);
-	var imgName = file.name; // not used, Irand just in case if user wanrand to print it.
-	var src		= e.target.result;
+create_box = function (e, file, size) {
 
-	var template = '<div class="eachImage" id="'+rand+'">';
-	template += '<span class="preview" id="'+rand+'"><img src="'+src+'"><span class="overlay"><span class="updone"></span></span>';
-	template += '</span>';
+  var rand = Math.floor((Math.random() * 100000) + 3);
+  var imgName = file.name; // not used, Irand just in case if user wanrand to print it.
+  var src = e.target.result;
+
+  var template = '<div class="eachImage" id="' + rand + '">';
+  template += '<span class="preview" id="' + rand + '"><img src="' + src + '"><span class="overlay"><span class="updone"></span></span>';
+  template += '</span>';
 //	template += '<div class="progress" id="'+rand+'"><span></span></div>';
-  template += '<div class="result" id="'+rand+'"><span></span></div>';
+  var x ;
+  var height = (200/size.h);// * test.h;
+  var width = (200/size.w);// * test.w;
+  if(height < width){
+    x = height;
+  } else {
+    x = width;
+  }
+  size.hPro = size.h *x;
+  size.wPro = size.w *x;
+  template += '<div class="resultPreview" id="resultPreview-' + rand + '" style="position:fixed;width:' + size.wPro+ 'px;height:' +  size.hPro+ 'px;background-color: #fff;" id="' + rand + '"></div>';
 
-	if($("#dropbox .eachImage").html() == null)
-		$("#dropbox").html(template);
-	else
-		$("#dropbox").append(template);
+  if ($("#dropbox .eachImage").html() == null)
+    $("#dropbox").html(template);
+  else
+    $("#dropbox").append(template);
 
-	// upload image
-	upload(file,rand);
+  // upload image
+  upload(file, rand);
 }
 
-upload = function(file,rand){
+upload = function (file, rand) {
 
 
   var formData = new FormData();
   formData.append('file', file);
 
-/*  $.ajax({
-    type:'POST',
-    url: 'http://54.208.187.238/Camera51Server/uploadimage',
-    data: formData,
-    cache: false,
-    contentType: false,
-    processData: false,
-    success:function(data) {
-    },
-    error: function(data) {
+  // now upload the file
+  var xhr = new Array();
+  xhr[rand] = new XMLHttpRequest();
+  xhr[rand].open("post", "http://api.malabi.co/Camera51Server/uploadimage", true);
+
+  xhr[rand].upload.addEventListener("progress", function (event) {
+    //console.log(event);
+    if (event.lengthComputable) {
+      $(".progress[id='" + rand + "'] span").css("width", (event.loaded / event.total) * 100 + "%");
+      $(".preview[id='" + rand + "'] .updone").html(((event.loaded / event.total) * 100).toFixed(2) + "%");
     }
-  });
-  return;*/
-	// now upload the file
-	var xhr = new Array();
-	xhr[rand] = new XMLHttpRequest();
-	xhr[rand].open("post", "http://api.malabi.co/Camera51Server/uploadimage", true);
+    else {
+      alert("Failed to compute file upload length");
+    }
+  }, false);
 
-	xhr[rand].upload.addEventListener("progress", function (event) {
-		//console.log(event);
-		if (event.lengthComputable) {
-			$(".progress[id='"+rand+"'] span").css("width",(event.loaded / event.total) * 100 + "%");
-			$(".preview[id='"+rand+"'] .updone").html(((event.loaded / event.total) * 100).toFixed(2)+"%");
-		}
-		else {
-			alert("Failed to compute file upload length");
-		}
-	}, false);
-
-	xhr[rand].onreadystatechange = function (oEvent) {
-	  if (xhr[rand].readyState === 4) {
-		if (xhr[rand].status === 200) {
-		  $(".progress[id='"+rand+"'] span").css("width","100%");
-		  $(".preview[id='"+rand+"']").find(".updone").html("100%");
-		  $(".preview[id='"+rand+"'] .overlay").css("display","none");
-      console.log("done",xhr[rand].responseText);
-      uploadDone(rand,xhr[rand].responseText );
-		} else {
-		  alert("Error : Unexpected error while uploading file");
-		}
-	  }
-	};
-	// Set headers
-	/*xhr[rand].setRequestHeader("Content-Type", "multipart/form-data");
-	xhr[rand].setRequestHeader("X-File-Name", file.fileName);
-	xhr[rand].setRequestHeader("X-File-Size", file.fileSize);
-	xhr[rand].setRequestHeader("X-File-Type", file.type);
-*/
+  xhr[rand].onreadystatechange = function (oEvent) {
+    if (xhr[rand].readyState === 4) {
+      if (xhr[rand].status === 200) {
+        $(".progress[id='" + rand + "'] span").css("width", "100%");
+        $(".preview[id='" + rand + "']").find(".updone").html("100%");
+        $(".preview[id='" + rand + "'] .overlay").css("display", "none");
+        console.log("done", xhr[rand].response);
+        data = JSON.parse(xhr[rand].responseText);
+        uploadDone(rand, data.uploadUrl);
+      } else {
+        alert("Error : Unexpected error while uploading file");
+      }
+    }
+  };
+  // Set headers
+  /*xhr[rand].setRequestHeader("Content-Type", "multipart/form-data");
+   xhr[rand].setRequestHeader("X-File-Name", file.fileName);
+   xhr[rand].setRequestHeader("X-File-Size", file.fileSize);
+   xhr[rand].setRequestHeader("X-File-Type", file.type);
+   */
   //xhr[rand].setRequestHeader("Content-Type", "multipart/form-data" );
 
-	// Send the file (doh)
-	xhr[rand].send(formData);
+  // Send the file (doh)
+  xhr[rand].send(formData);
 
 }
